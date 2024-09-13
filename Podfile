fcba08ee669eb1688202b2c0805896df95686e58
Dhruv Lakdawala <dhruv.l.easternts@gmail.com>
2024-09-13 12:02:07 +0530

    pod installation of SampleFrameworkLibrary, SVProgressHUD, Dropdown & utilize it.

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SampleDemoPodApp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SampleDemoPodApp
+  pod 'SampleFrameworkLibrary', :git => 'https://github.com/dhruv230298/SampleDemoApp.git', :branch => 'main'
+  pod 'SVProgressHUD'
+  pod 'DropDown'   
+
+end
---
+++ Pods/SampleFrameworkLibrary/SDK/SampleFrameworkLibrary.xcframework/ios-arm64_x86_64-simulator/SampleFrameworkLibrary.framework/Headers/SampleFrameworkLibrary.h
@@ -0,0 +1,18 @@
+//
+//  SampleFrameworkLibrary.h
+//  SampleFrameworkLibrary
+//
+//  Created by user on 13/09/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SampleFrameworkLibrary.
+FOUNDATION_EXPORT double SampleFrameworkLibraryVersionNumber;
+
+//! Project version string for SampleFrameworkLibrary.
+FOUNDATION_EXPORT const unsigned char SampleFrameworkLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleFrameworkLibrary/PublicHeader.h>
+
+
